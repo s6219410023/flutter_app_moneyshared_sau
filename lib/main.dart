9d8d254b0e17bebc0453280ded78258c5f944e03
s6219410023 <s6219410023@sau.ac.th>
2022-02-05 19:12:48 +0700

    Code v.1: do xxxxxx

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_moneyshared_sau/views/moneyshare_ui.dart';
+
+
+main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MoneyshareUI(),
+      theme: ThemeData(
+        fontFamily: 'Kanit',
+      ),
+    ),
+  );
+}
---
+++ lib/views/show_moneyshare_ui.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:matcher/matcher.dart';
+
+class ShowmoneyshareUI extends StatefulWidget {
+  const ShowmoneyshareUI({Key? key}) : super(key: key);
+
+  @override
+  _ShowmoneyshareUIState createState() => _ShowmoneyshareUIState();
+}
+
+class _ShowmoneyshareUIState extends State<ShowmoneyshareUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF2D9F7),
+      body: SingleChildScrollView(),
+    );
+  }
+}
